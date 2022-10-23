87d8a2244dd6c4a53b959d2ef892a28d3eb5be1d
Marsa <mgomezlavall@gmail.com>
2022-10-23 23:23:18 +0200

    compares in utc as well

--- src/pages/api/calendar.ts
+++ src/pages/api/calendar.ts
@@ -13,15 +13,17 @@ export default async function handler(req, res) {
         res.status(200).json({ data: m });
         // Process a POST request
     } else {
-        let day = dayjs().subtract(8, 'month').utc();
+        let day = dayjs().utc(true).subtract(8, 'month');
         const calendar = await prisma.moodCalendar.findFirst();
         const days = await prisma.moodDay.findMany({ where: { moodCalendarId: calendar.id } });
         let data = [];
 
         do {
-            const sameDay = days.find((a) => dayjs(a.date).isSame(day, "day"))
+            const sameDay = days.find((a) => dayjs.utc(a.date).isSame(day, "day"))
 
             if (sameDay) {
+                console.log(day);
+                console.log(dayjs(days[0].date));
                 data.push({ ...sameDay, date: dayjs(sameDay.date).format("YYYY-MM-DD") })
             } else {
                 data.push({ rating: null, date: day.format("YYYY-MM-DD") });
